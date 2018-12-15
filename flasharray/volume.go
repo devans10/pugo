b62520d34a012e9b7890e722d26b3130661d423a
Dave Evans <devans@duqlight.com>
2018-12-15 17:35:49 -0500

    Added Volume acceptance tests Fixed a bug in EradicateVolume added suffix to CreateSnapshot(s)

--- flasharray/volume.go
+++ flasharray/volume.go
@@ -33,9 +33,9 @@ func (v *VolumeService) SetVolume(name string, params map[string]string, data in
 }
 
 // CreateSnapshot function creates a volume snapshot of the volume passed in the argument.
-func (v *VolumeService) CreateSnapshot(volume string, params map[string]string) (*Volume, error) {
+func (v *VolumeService) CreateSnapshot(volume string, suffix string, params map[string]string) (*Volume, error) {
 	volumes := []string{volume}
-	m, err := v.CreateSnapshots(volumes, nil)
+	m, err := v.CreateSnapshots(volumes, suffix, params)
 	if err != nil {
 		return nil, err
 	}
@@ -45,15 +45,17 @@ func (v *VolumeService) CreateSnapshot(volume string, params map[string]string) 
 
 // CreateSnapshosts function will create a snapshot of all the volumes passed in the volumes slice.
 // an array of volume objects is returned.
-func (v *VolumeService) CreateSnapshots(volumes []string, params map[string]string) ([]Volume, error) {
+func (v *VolumeService) CreateSnapshots(volumes []string, suffix string, params map[string]string) ([]Volume, error) {
 	type data struct {
-		Snap   bool     `json:"snap"`
-		Source []string `json:"source"`
+		Snap   bool     `json:"snap,omitempty"`
+		Suffix string   `json:"suffix,omitempty"`
+		Source []string `json:"source,omitepty"`
 	}
 
 	d := &data{}
 	d.Snap = true
 	d.Source = volumes
+	d.Suffix = suffix
 	req, err := v.client.NewRequest("POST", "volume", params, d)
 	if err != nil {
 		return nil, err
@@ -164,7 +166,7 @@ func (v *VolumeService) EradicateVolume(name string, params map[string]string) (
 
 	path := fmt.Sprintf("volume/%s", name)
 	data := map[string]bool{"eradicate": true}
-	req, err := v.client.NewRequest("POST", path, params, data)
+	req, err := v.client.NewRequest("DELETE", path, params, data)
 	if err != nil {
 		return nil, err
 	}
---
+++ flasharray/volume_test.go
@@ -0,0 +1,183 @@
+// Copyright 2018 Dave Evans. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package flasharray
+
+import (
+	"fmt"
+	"testing"
+)
+
+const testAccVolumeName = "testAccvolume"
+const testvolsnapshot = "testaccvolsnapshot"
+const testvolclone = "testaccvolcone"
+const testvolsize = "1G"
+const testvolresize = "2G"
+const testpgroup = "testacchostpgroup"
+
+func TestAccVolumes(t *testing.T) {
+	testAccPreChecks(t)
+	c := testAccGenerateClient(t)
+
+	c.Protectiongroups.CreateProtectiongroup(testpgroup, nil, nil)
+
+	t.Run("CreateVolume", testAccCreateVolume(c))
+	t.Run("GetVolume", testAccGetVolume(c))
+	t.Run("CreateSnapshot", testAccCreateSnapshot(c))
+	t.Run("CloneVolume", testAccCloneVolume(c))
+	t.Run("AddVolumeToPgroup", testAccAddVolume(c, testpgroup))
+	t.Run("RemoveVolumeFromPgroup", testAccRemoveVolume(c, testpgroup))
+	t.Run("ExtendVolume", testAccExtendVolume(c))
+	t.Run("TruncateVolume", testAccTruncateVolume(c))
+	t.Run("ListVolumes", testAccListVolumes(c))
+	t.Run("RenameVolume", testAccRenameVolume(c, "testAccVolnew"))
+	c.Volumes.RenameVolume("testAccVolnew", testAccVolumeName, nil)
+	t.Run("DeleteVolume", testAccDeleteVolume(c))
+	t.Run("RecoverVolume", testAccRecoverVolume(c))
+	c.Volumes.DeleteVolume(testAccVolumeName, nil)
+	t.Run("EradicateVolume", testAccEradicateVolume(c))
+
+	c.Volumes.DeleteVolume(testvolclone, nil)
+	c.Volumes.DeleteVolume(fmt.Sprintf("%s.%s", testAccVolumeName, testvolsnapshot), nil)
+	c.Volumes.EradicateVolume(testvolclone, nil)
+	c.Volumes.EradicateVolume(fmt.Sprintf("%s.%s", testAccVolumeName, testvolsnapshot), nil)
+	c.Protectiongroups.DestroyProtectiongroup(testpgroup, nil)
+	c.Protectiongroups.EradicateProtectiongroup(testpgroup, nil)
+}
+
+func testAccCreateVolume(c *Client) func(*testing.T) {
+	return func(t *testing.T) {
+		h, err := c.Volumes.CreateVolume(testAccVolumeName, testvolsize, nil)
+		if err != nil {
+			t.Fatalf("error creating volume %s: %s", testAccVolumeName, err)
+		}
+
+		if h.Name != testAccVolumeName {
+			t.Fatalf("expected: %s; got %s", testAccVolumeName, h.Name)
+		}
+	}
+}
+
+func testAccGetVolume(c *Client) func(*testing.T) {
+	return func(t *testing.T) {
+		h, err := c.Volumes.GetVolume(testAccVolumeName, nil)
+		if err != nil {
+			t.Fatalf("error getting volume %s: %s", testAccVolumeName, err)
+		}
+
+		if h.Name != testAccVolumeName {
+			t.Fatalf("expected: %s; got %s", testAccVolumeName, h.Name)
+		}
+	}
+}
+
+func testAccCreateSnapshot(c *Client) func(*testing.T) {
+	return func(t *testing.T) {
+		h, err := c.Volumes.CreateSnapshot(testAccVolumeName, testvolsnapshot, nil)
+		if err != nil {
+			t.Fatalf("error snapshotting volume %s: %s", testAccVolumeName, err)
+		}
+
+		if h.Name != fmt.Sprintf("%s.%s", testAccVolumeName, testvolsnapshot) {
+			t.Fatalf("expected: %s; got %s", fmt.Sprintf("%s.%s", testAccVolumeName, testvolsnapshot), h.Name)
+		}
+	}
+}
+
+func testAccCloneVolume(c *Client) func(*testing.T) {
+	return func(t *testing.T) {
+		h, err := c.Volumes.CopyVolume(testvolclone, testAccVolumeName, nil)
+		if err != nil {
+			t.Fatalf("error cloning volume %s: %s", testAccVolumeName, err)
+		}
+
+		if h.Name != testvolclone {
+			t.Fatalf("expected: %s; got %s", testvolclone, h.Name)
+		}
+		if h.Source != testAccVolumeName {
+			t.Fatalf("expected: %s; got %s", testAccVolumeName, h.Source)
+		}
+	}
+}
+
+func testAccAddVolume(c *Client, pgroup string) func(*testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.AddVolume(testAccVolumeName, pgroup, nil)
+		if err != nil {
+			t.Fatalf("error adding volume %s to pgroup %s: %s", testAccVolumeName, pgroup, err)
+		}
+	}
+}
+
+func testAccRemoveVolume(c *Client, pgroup string) func(*testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.RemoveVolume(testAccVolumeName, pgroup, nil)
+		if err != nil {
+			t.Fatalf("error removing volume %s from pgroup %s: %s", testAccVolumeName, pgroup, err)
+		}
+	}
+}
+
+func testAccListVolumes(c *Client) func(*testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.ListVolumes(nil)
+		if err != nil {
+			t.Fatalf("error listing volumes: %s", err)
+		}
+	}
+}
+
+func testAccExtendVolume(c *Client) func(*testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.ExtendVolume(testAccVolumeName, testvolresize, nil)
+		if err != nil {
+			t.Fatalf("error extending volume %s to %s: %s", testAccVolumeName, testvolresize, err)
+		}
+	}
+}
+
+func testAccTruncateVolume(c *Client) func(*testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.TruncateVolume(testAccVolumeName, testvolsize, nil)
+		if err != nil {
+			t.Fatalf("error truncating volume %s to %s: %s", testAccVolumeName, testvolsize, err)
+		}
+	}
+}
+
+func testAccRenameVolume(c *Client, name string) func(*testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.RenameVolume(testAccVolumeName, name, nil)
+		if err != nil {
+			t.Fatalf("error renaming volume %s to %s: %s", testAccVolumeName, name, err)
+		}
+	}
+}
+
+func testAccDeleteVolume(c *Client) func(t *testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.DeleteVolume(testAccVolumeName, nil)
+		if err != nil {
+			t.Fatalf("error deleting volume: %s", err)
+		}
+	}
+}
+
+func testAccRecoverVolume(c *Client) func(t *testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.RecoverVolume(testAccVolumeName, nil)
+		if err != nil {
+			t.Fatalf("error recovering volume %s: %s", testAccVolumeName, err)
+		}
+	}
+}
+
+func testAccEradicateVolume(c *Client) func(t *testing.T) {
+	return func(t *testing.T) {
+		_, err := c.Volumes.EradicateVolume(testAccVolumeName, nil)
+		if err != nil {
+			t.Fatalf("error eradicating volume: %s", err)
+		}
+	}
+}
