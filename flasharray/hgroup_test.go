bc9de6ce9bb1e736eda50fd69bc5e750f7fe60c1
Dave Evans <devans@fastmail.com>
2019-04-06 13:34:40 -0400

    Added Hgroup Unit Tests

--- flasharray/hgroup_test.go
+++ flasharray/hgroup_test.go
@@ -5,6 +5,9 @@
 package flasharray
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
@@ -215,4 +218,201 @@ func testAccDeleteHostgroup(c *Client) func(t *testing.T) {
 			t.Fatalf("error deleting hostgroup: %s", err)
 		}
 	}
+}
+
+func TestConnectHostgroup(t *testing.T) {
+	restVersion := "1.15"
+	testConn := ConnectedVolume{Vol: "v3", Name: "hg3", Lun: 254}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg3/volume/v3", req.URL.String())
+		equals(t, "POST", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPostHgrouphgroupVolumevol(restVersion))),
+			Header:     head,
+		}
+	})
+
+	conn, err := c.Hostgroups.ConnectHostgroup("hg3", "v3", nil)
+	ok(t, err)
+	equals(t, &testConn, conn)
+}
+
+func TestCreateHostgroup(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := Hostgroup{Name: "hg3", Hosts: []string{}}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/h3", req.URL.String())
+		equals(t, "POST", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPostHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	hgroup, err := c.Hostgroups.CreateHostgroup("h3", testHostgroup)
+	ok(t, err)
+	equals(t, &testHostgroup, hgroup)
+}
+
+func TestDeleteHostgroup(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := Hostgroup{Name: "hg4"}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/h4", req.URL.String())
+		equals(t, "DELETE", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respDeleteHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	hgroup, err := c.Hostgroups.DeleteHostgroup("h4")
+	ok(t, err)
+	equals(t, &testHostgroup, hgroup)
+}
+
+func TestDisconnectHostgroup(t *testing.T) {
+	restVersion := "1.15"
+	testConn := ConnectedVolume{Vol: "v3", Name: "hg4"}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg4/volume/v3", req.URL.String())
+		equals(t, "DELETE", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respDeleteHgrouphgroupVolumevol(restVersion))),
+			Header:     head,
+		}
+	})
+
+	conn, err := c.Hostgroups.DisconnectHostgroup("hg4", "v3")
+	ok(t, err)
+	equals(t, &testConn, conn)
+}
+
+func TestGetHostgroup(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := Hostgroup{Name: "hg1", Hosts: []string{"h1", "h2"}}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg1", req.URL.String())
+		equals(t, "GET", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respGetHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	hgroup, err := c.Hostgroups.GetHostgroup("hg1", nil)
+	ok(t, err)
+	equals(t, &testHostgroup, hgroup)
+}
+
+//TODO: get ouput
+//func TestAddHostgroup(t *testing.T) {}
+
+//TODO: get ouput
+//func TestRemoveHostgroup(t *testing.T) {}
+
+func TestGetHostgroupConnections(t *testing.T) {
+	restVersion := "1.15"
+	testConn := []HostgroupConnection{HostgroupConnection{Name: "hg1", Vol: "v1", Lun: 254}}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg1/volume", req.URL.String())
+		equals(t, "GET", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respGetHgrouphgroupVolume(restVersion))),
+			Header:     head,
+		}
+	})
+
+	conn, err := c.Hostgroups.ListHostgroupConnections("hg1")
+	ok(t, err)
+	equals(t, testConn, conn)
+}
+
+func TestListHostgroups(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := []Hostgroup{Hostgroup{Name: "hg1", Hosts: []string{"h1", "h2"}},
+		Hostgroup{Name: "hg2", Hosts: []string{}},
+	}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup", req.URL.String())
+		equals(t, "GET", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respGetHgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	hgroup, err := c.Hostgroups.ListHostgroups(nil)
+	ok(t, err)
+	equals(t, testHostgroup, hgroup)
+}
+
+func TestRenameHostgroup(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := Hostgroup{Name: "hg4_renamed", Hosts: []string{"h3"}}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg4", req.URL.String())
+		equals(t, "PUT", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPutHgrouphgroupRename(restVersion))),
+			Header:     head,
+		}
+	})
+
+	hgroup, err := c.Hostgroups.RenameHostgroup("hg4", "hg4_renamed")
+	ok(t, err)
+	equals(t, &testHostgroup, hgroup)
+}
+
+func TestSetHostgroup(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := Hostgroup{Name: "hg4", Hosts: []string{"h3"}}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg4", req.URL.String())
+		equals(t, "PUT", req.Method)
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPutHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	hgroup, err := c.Hostgroups.SetHostgroup("hg4", testHostgroup)
+	ok(t, err)
+	equals(t, &testHostgroup, hgroup)
 }
