8739acd7fbb237e50e18b1055f8279f28f59ef4b
Dave Evans <devans@fastmail.com>
2019-04-06 14:03:25 -0400

    Added hgroup unit error test

--- flasharray/hgroup_test.go
+++ flasharray/hgroup_test.go
@@ -241,6 +241,27 @@ func TestConnectHostgroup(t *testing.T) {
 	equals(t, &testConn, conn)
 }
 
+func TestConnectHostgroupError(t *testing.T) {
+	restVersion := "1.15"
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg3/volume/v3", req.URL.String())
+		equals(t, "POST", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPostHgrouphgroupVolumevol(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.ConnectHostgroup("hg3", "v3", nil)
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
+}
+
 func TestCreateHostgroup(t *testing.T) {
 	restVersion := "1.15"
 	testHostgroup := Hostgroup{Name: "hg3", Hosts: []string{}}
@@ -260,6 +281,28 @@ func TestCreateHostgroup(t *testing.T) {
 	hgroup, err := c.Hostgroups.CreateHostgroup("h3", testHostgroup)
 	ok(t, err)
 	equals(t, &testHostgroup, hgroup)
+}
+
+func TestCreateHostgroupError(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := Hostgroup{Name: "hg3", Hosts: []string{}}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/h3", req.URL.String())
+		equals(t, "POST", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPostHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.CreateHostgroup("h3", testHostgroup)
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
 }
 
 func TestDeleteHostgroup(t *testing.T) {
@@ -283,6 +326,27 @@ func TestDeleteHostgroup(t *testing.T) {
 	equals(t, &testHostgroup, hgroup)
 }
 
+func TestDeleteHostgroupError(t *testing.T) {
+	restVersion := "1.15"
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/h4", req.URL.String())
+		equals(t, "DELETE", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respDeleteHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.DeleteHostgroup("h4")
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
+}
+
 func TestDisconnectHostgroup(t *testing.T) {
 	restVersion := "1.15"
 	testConn := ConnectedVolume{Vol: "v3", Name: "hg4"}
@@ -304,6 +368,27 @@ func TestDisconnectHostgroup(t *testing.T) {
 	equals(t, &testConn, conn)
 }
 
+func TestDisconnectHostgroupError(t *testing.T) {
+	restVersion := "1.15"
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg4/volume/v3", req.URL.String())
+		equals(t, "DELETE", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respDeleteHgrouphgroupVolumevol(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.DisconnectHostgroup("hg4", "v3")
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
+}
+
 func TestGetHostgroup(t *testing.T) {
 	restVersion := "1.15"
 	testHostgroup := Hostgroup{Name: "hg1", Hosts: []string{"h1", "h2"}}
@@ -323,6 +408,27 @@ func TestGetHostgroup(t *testing.T) {
 	hgroup, err := c.Hostgroups.GetHostgroup("hg1", nil)
 	ok(t, err)
 	equals(t, &testHostgroup, hgroup)
+}
+
+func TestGetHostgroupError(t *testing.T) {
+	restVersion := "1.15"
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg1", req.URL.String())
+		equals(t, "GET", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respGetHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.GetHostgroup("hg1", nil)
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
 }
 
 //TODO: get ouput
@@ -352,6 +458,27 @@ func TestGetHostgroupConnections(t *testing.T) {
 	equals(t, testConn, conn)
 }
 
+func TestGetHostgroupConnectionsError(t *testing.T) {
+	restVersion := "1.15"
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg1/volume", req.URL.String())
+		equals(t, "GET", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respGetHgrouphgroupVolume(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.ListHostgroupConnections("hg1")
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
+}
+
 func TestListHostgroups(t *testing.T) {
 	restVersion := "1.15"
 	testHostgroup := []Hostgroup{Hostgroup{Name: "hg1", Hosts: []string{"h1", "h2"}},
@@ -375,6 +502,27 @@ func TestListHostgroups(t *testing.T) {
 	equals(t, testHostgroup, hgroup)
 }
 
+func TestListHostgroupsError(t *testing.T) {
+	restVersion := "1.15"
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup", req.URL.String())
+		equals(t, "GET", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respGetHgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.ListHostgroups(nil)
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
+}
+
 func TestRenameHostgroup(t *testing.T) {
 	restVersion := "1.15"
 	testHostgroup := Hostgroup{Name: "hg4_renamed", Hosts: []string{"h3"}}
@@ -396,6 +544,27 @@ func TestRenameHostgroup(t *testing.T) {
 	equals(t, &testHostgroup, hgroup)
 }
 
+func TestRenameHostgroupError(t *testing.T) {
+	restVersion := "1.15"
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg4", req.URL.String())
+		equals(t, "PUT", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPutHgrouphgroupRename(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.RenameHostgroup("hg4", "hg4_renamed")
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
+}
+
 func TestSetHostgroup(t *testing.T) {
 	restVersion := "1.15"
 	testHostgroup := Hostgroup{Name: "hg4", Hosts: []string{"h3"}}
@@ -415,4 +584,26 @@ func TestSetHostgroup(t *testing.T) {
 	hgroup, err := c.Hostgroups.SetHostgroup("hg4", testHostgroup)
 	ok(t, err)
 	equals(t, &testHostgroup, hgroup)
+}
+
+func TestSetHostgroupError(t *testing.T) {
+	restVersion := "1.15"
+	testHostgroup := Hostgroup{Name: "hg4", Hosts: []string{"h3"}}
+	head := make(http.Header)
+	head.Add("Content-Type", "application/json")
+
+	c := testGenerateClient(func(req *http.Request) *http.Response {
+		equals(t, "https://flasharray.example.com/api/1.15/hgroup/hg4", req.URL.String())
+		equals(t, "PUT", req.Method)
+		return &http.Response{
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(respPutHgrouphgroup(restVersion))),
+			Header:     head,
+		}
+	})
+
+	_, err := c.Hostgroups.SetHostgroup("hg4", testHostgroup)
+	if err == nil {
+		t.Errorf("error not returned on 500 response")
+	}
 }
